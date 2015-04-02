3d94690f15b9807b9e9376104ba53f431cbcbbbd
RPL <r@rpl.im>
2015-04-02 14:51:14 +0200

    tree  again

--- modules/angular/app.js
+++ modules/angular/app.js
@@ -5,3 +5,99 @@ app.filter('capitalize', function() {
         return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
     }
 });
+
+app.controller('TreeController', function($scope, $http, $rootScope) {
+    $scope.treeData = {
+        course: {
+            name: 'Web Tech',
+            subTopics:[
+                {
+                    name: 'Server Side Technology',
+                    resources:[
+                        {
+                            type: 'pdf'
+                        },{
+                            type: 'pdf'
+                        },{
+                            type: 'video'
+                        }
+                    ],
+                    lessons: [
+                        {
+                            name: 'Introduction to server Side Technology',
+                            resources: [
+                                {
+                                    type: 'pdf'
+                                }
+                            ]
+                        },
+                        {
+                            name: 'Distributed Server',
+                            resources: [
+                                {
+                                    type: 'video'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: 'Client Side Technology',
+                    resources:[
+                        {
+                            type: 'pdf'
+                        },{
+                            type: 'pdf'
+                        },{
+                            type: 'pdf'
+                        }
+                    ],
+                    lessons:[
+                        {
+                            name: 'Introduction to Client Side Technology',
+                            resources: [
+                                {
+                                    type: 'pdf'
+                                }
+                            ]
+                        },
+                        {
+                            name: 'Web Standards',
+                            resources: [
+                                {
+                                    type: 'pdf'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+
+            resources:[
+                {
+                    type: 'pdf'
+                },
+                {
+                    type: 'video'
+                }
+
+            ]
+        }
+    };
+
+    var m = [20, 120, 20, 120],
+        w = 1280 - m[1] - m[3],
+        h = 800 - m[0] - m[2],
+        i = 0;
+
+    var vis = d3.select('#tree').append('svg:svg')
+        .attr('width', w + m[1] + m[3])
+        .attr('height', h + m[0] + m[2])
+        .append('svg:g')
+        .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
+
+    var node = vis.selectAll('g.node')
+        .data($scope.treeData)
+        .enter()
+        .append('g.node');
+});
--- public/angular/courseMapper.js
+++ public/angular/courseMapper.js
@@ -4,6 +4,102 @@ app.filter('capitalize', function() {
     return function(input, all) {
         return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
     }
+});
+
+app.controller('TreeController', function($scope, $http, $rootScope) {
+    $scope.treeData = {
+        course: {
+            name: 'Web Tech',
+            subTopics:[
+                {
+                    name: 'Server Side Technology',
+                    resources:[
+                        {
+                            type: 'pdf'
+                        },{
+                            type: 'pdf'
+                        },{
+                            type: 'video'
+                        }
+                    ],
+                    lessons: [
+                        {
+                            name: 'Introduction to server Side Technology',
+                            resources: [
+                                {
+                                    type: 'pdf'
+                                }
+                            ]
+                        },
+                        {
+                            name: 'Distributed Server',
+                            resources: [
+                                {
+                                    type: 'video'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: 'Client Side Technology',
+                    resources:[
+                        {
+                            type: 'pdf'
+                        },{
+                            type: 'pdf'
+                        },{
+                            type: 'pdf'
+                        }
+                    ],
+                    lessons:[
+                        {
+                            name: 'Introduction to Client Side Technology',
+                            resources: [
+                                {
+                                    type: 'pdf'
+                                }
+                            ]
+                        },
+                        {
+                            name: 'Web Standards',
+                            resources: [
+                                {
+                                    type: 'pdf'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+
+            resources:[
+                {
+                    type: 'pdf'
+                },
+                {
+                    type: 'video'
+                }
+
+            ]
+        }
+    };
+
+    var m = [20, 120, 20, 120],
+        w = 1280 - m[1] - m[3],
+        h = 800 - m[0] - m[2],
+        i = 0;
+
+    var vis = d3.select('#tree').append('svg:svg')
+        .attr('width', w + m[1] + m[3])
+        .attr('height', h + m[0] + m[2])
+        .append('svg:g')
+        .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
+
+    var node = vis.selectAll('g.node')
+        .data($scope.treeData)
+        .enter()
+        .append('g.node');
 });;app.controller('CategoryListController', function($scope, $http, $rootScope) {
 
   $http.get('/api/catalogs/categories').success(function(data) {
@@ -25,4 +121,33 @@ app.controller('CourseListController', function($scope, $http, $rootScope) {
         $scope.user = data;
         $rootScope.user = data;
     });
+});
+
+app.controller('RightClickMenuController', function($scope, $http, $rootScope) {
+    $scope.createTopic = function(name){
+        /*
+        if(!$rootScope.tree)
+            $rootScope.tree = {};
+
+        $rootScope.tree.course = {
+            name: name,
+            subTopics: [],
+            resources:[]
+        };*/
+
+        console.log("creating topic");
+    };
+
+    $scope.createSubTopic = function(name, topic){
+        /*
+        if(topic){
+            topic.push()
+        }
+
+        $rootScope.tree.course.subTopics.push({
+
+        });
+        */
+        console.log("creating sub topic");
+    }
 });
