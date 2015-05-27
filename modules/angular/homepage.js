8e16f8a055cd90a4508434bbd103a07bef23d013
RPL <r@rpl.im>
2015-05-27 22:50:27 +0200

    use js plumb as our graph librara

--- modules/angular/homepage.js
+++ modules/angular/homepage.js
@@ -13,7 +13,16 @@ app.controller('HomePageController', function($scope, $http, $rootScope) {
         $scope.height = jQuery(window).height();
         $scope.center = {x:$scope.width/2, y: $scope.height/2};
 
+    });
 
+    /**
+     * get all categories, recursived on the server
+     */
+    $http.get('/api/catalogs/categories').success(function (data) {
+        if(data.categories)
+            $scope.categories = data.categories;
+        else
+            $scope.categories = false;
     });
 
     $scope.setHideSlider = function(){
@@ -37,23 +46,67 @@ app.controller('HomePageController', function($scope, $http, $rootScope) {
         var instance = jsPlumb.getInstance({
             Endpoint: ["Dot", {radius: 2}],
             HoverPaintStyle: {strokeStyle: "#1e8151", lineWidth: 2 },
-            ConnectionOverlays: [
-                [ "Arrow", {
-                    location: 1,
-                    id: "arrow",
-                    length: 14,
-                    foldback: 0.8
-                } ],
-
-            ],
+            ConnectionOverlays: [ ],
             Container: "category-map"
         });
 
-        instance.connect({ source: "opened", target: "phone1" });
-        instance.connect({ source: "phone1", target: "phone1" });
-        instance.connect({ source: "phone1", target: "inperson" });
+        // let us drag and drop the cats
+        var mapEl = jsPlumb.getSelector(".category-map .w");
+        instance.draggable(mapEl);
 
-        window.jsp = instance;
+        instance.bind("click", function (c) {
+            instance.detach(c);
+        });
+
+        // initialise all '.w' elements as connection targets.
+        instance.batch(function () {
+            /*instance.makeSource(mapEl, {
+                filter: ".ep",
+                anchor: "Continuous",
+                connector: [ "StateMachine", { curviness: 20 } ],
+                connectorStyle: { strokeStyle: "#5c96bc", lineWidth: 2, outlineColor: "transparent", outlineWidth: 4 },
+                maxConnections: 5,
+                onMaxConnections: function (info, e) {
+                    alert("Maximum connections (" + info.maxConnections + ") reached");
+                }
+            });*/
+
+            /*instance.makeTarget(mapEl, {
+                dropOptions: {hoverClass: "dragHover"},
+                anchor: "Continuous",
+                allowLoopback: true
+            });*/
+
+            /* connect center to first level cats*/
+            for(var i in $scope.categories){
+                var cat = $scope.categories[i];
+                instance.connect({
+                    source: 'center', target: cat.slug,
+                    anchors: [
+                        [ "Perimeter", { shape: jsPlumb.getSelector('#center')[0].getAttribute("data-shape") }],
+                        [ "Perimeter", { shape: jsPlumb.getSelector('#'+cat.slug)[0].getAttribute("data-shape") }]
+                    ]
+                });
+                $scope.interConnect(cat, instance);
+            }
+        });
+    };
+
+    $scope.interConnect = function(category, instance){
+        if(category.subCategories){
+            for(var i in category.subCategories){
+                var child = category.subCategories[i];
+                instance.connect({
+                    source: category.slug, target: child.slug,
+                    anchors: [
+                        [ "Perimeter", { shape: jsPlumb.getSelector('#'+category.slug)[0].getAttribute("data-shape") }],
+                        [ "Perimeter", { shape: jsPlumb.getSelector('#'+child.slug)[0].getAttribute("data-shape") }]
+                    ]
+                });
+
+                $scope.interConnect(child, instance);
+            }
+        }
     }
 
 });
