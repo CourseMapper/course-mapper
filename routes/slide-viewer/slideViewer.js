9a830bbbbbf791a7edaea4b0da082a81a908b574
Kaethon <johanneskloeckner@web.de>
2015-06-21 23:23:19 +0200

    test comment retrieval

--- routes/slide-viewer/slideViewer.js
+++ routes/slide-viewer/slideViewer.js
@@ -16,4 +16,14 @@ router.post('/slide-viewer', function(req, res, next){
     comment.handleSubmitPost(req, res, next);
 });
 
+/*router.get('/slide-viewer/displayComments', function(req, res, next) {
+    res.render('index', {
+      title: 'Test',
+      pagetitle: 'Hello',
+      comments: comment
+
+    });
+});*/
+
+
 module.exports = router;
