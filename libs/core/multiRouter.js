328408ae57272c2c032caa03358d0639eb59a0a6
RPL <r@rpl.im>
2015-07-01 13:18:19 +0200

    fixed multiRouter works on windows

--- libs/core/multiRouter.js
+++ libs/core/multiRouter.js
@@ -15,23 +15,26 @@ multiRouter.prototype.populateRoutes = function(prefix){
     if (!prefix)
         prefix = '/';
 
-    var routeFiles = fs.readdirSync(this.directory + p.sep + prefix);
+    var routeFiles = fs.readdirSync(this.directory + '/' + prefix);
 
     for(var i in routeFiles){
         var routeFN = routeFiles[i];
 
         // variables for directory checking
-        var newPath = p.join(this.directory + p.sep + prefix, routeFN);
+        var newPath = p.join(this.directory + '/' + prefix, routeFN);
         var stats = fs.lstatSync(newPath);
 
         // check if it is a dir
         if(stats.isDirectory()){
-            debug('it is a dir, recall: ' + routeFN);
+            if(prefix == '/')
+                prefix = '';
+
+            debug('it is a dir, prefix: ' + (prefix + '/' + routeFN));
 
             // this is a directory, so we have to call this method again
-            this.populateRoutes(p.normalize(prefix + p.sep + routeFN));
+            this.populateRoutes(prefix + '/' + routeFN);
         } else {
-            var requiring = p.normalize(this.directory + p.sep + prefix + p.sep + routeFN);
+            var requiring = p.normalize(this.directory + '/' + prefix + '/' + routeFN);
             var route = require(requiring);
             this.app.use(prefix, route);
 
