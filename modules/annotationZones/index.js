6f4a2e417ecc69b0fb797f15ff7fefc40e0cc22e
Kaethon <johanneskloeckner@web.de>
2015-08-17 14:32:25 +0200

    Made annotationzone position and size 2 dimensional

--- modules/annotationZones/index.js
+++ modules/annotationZones/index.js
@@ -35,8 +35,14 @@ function submitSingleTag(tagList, err, restList, mainCallback, done) {
   //console.log(tagList);
   var annotationZonePDF = new AnnotationZonesPDF({
     annotationZoneName: tagList[0],
-    relativeCoordinates: tagList[1],
-    relativeDimensions: tagList[2],
+    relativeCoordinates: {
+      X: tagList[1].split(";")[0],
+      Y: tagList[1].split(";")[1]
+    },
+    relativeDimensions: {
+      X: tagList[2].split(";")[0],
+      Y: tagList[2].split(";")[1]
+    },
     color: tagList[3]
   });
 
