ac3a92d38f2ce6f6240c21460536c632564c800d
Kaethon <johanneskloeckner@web.de>
2015-08-30 22:43:27 +0200

    Added functionality to navigation buttons in slideviewer

--- frontend-modules/angular/pdfNavigation.controller.js
+++ frontend-modules/angular/pdfNavigation.controller.js
@@ -1,4 +1,10 @@
 app.controller('PDFNavigationController', function($scope, $http, $rootScope, $sce, $timeout) {
-    $scope.currentPageNumber = 0;
+    $scope.currentPageNumber = 1;
     $scope.maxPageNumber = 30;
+
+    $scope.changePageNumber = function(value){
+      console.log("GOT CALLED");
+      if( ($scope.currentPageNumber + value) <= $scope.maxPageNumber && ($scope.currentPageNumber + value) >= 1)
+        $scope.currentPageNumber = $scope.currentPageNumber + value;
+    }
 });
--- public/angular/courseMapper.js
+++ public/angular/courseMapper.js
@@ -1728,7 +1728,17 @@ app.controller('NewCourseController', function($scope, $filter, $http, $location
             });
     }
 });
-;;app.controller('AnnotationZoneListController', function($scope, $http, $rootScope, $sce, $timeout) {
+;app.controller('PDFNavigationController', function($scope, $http, $rootScope, $sce, $timeout) {
+    $scope.currentPageNumber = 1;
+    $scope.maxPageNumber = 30;
+
+    $scope.changePageNumber = function(value){
+      console.log("GOT CALLED");
+      if( ($scope.currentPageNumber + value) <= $scope.maxPageNumber && ($scope.currentPageNumber + value) >= 1)
+        $scope.currentPageNumber = $scope.currentPageNumber + value;
+    }
+});
+;app.controller('AnnotationZoneListController', function($scope, $http, $rootScope, $sce, $timeout) {
 
 
     $http.get('/slide-viewer/disAnnZones').success(function (data) {
