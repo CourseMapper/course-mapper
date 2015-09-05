42a49e7d86643fb838fc9176f069953c7f32a723
RPL <r@rpl.im>
2015-09-05 08:17:50 +0200

    create course form feedback

--- frontend-modules/angular/catalogs/courseNew.controller.js
+++ frontend-modules/angular/catalogs/courseNew.controller.js
@@ -1,5 +1,7 @@
 
 app.controller('NewCourseController', function($scope, $filter, $http, $location) {
+    $scope.submitted = false;
+
     $scope.course = {
         name: null,
         category: null,
@@ -7,38 +9,42 @@ app.controller('NewCourseController', function($scope, $filter, $http, $location
     };
 
     $scope.tagsRaw = null;
-    $scope.errorName = "";
+    $scope.errors = [];
 
     $scope.loadTags = function(query) {
         return $http.get('/api/category/' + $scope.category._id + '/courseTags?query=' + query);
     };
 
-    $scope.saveCourse = function() {
-        if($scope.tagsRaw) {
-            $scope.course.tags = JSON.stringify($scope.tagsRaw);
-        }
-        $scope.course.category = $scope.$parent.category._id;
+    $scope.saveCourse = function(isValid) {
+        if (isValid) {
 
-        var d = transformRequest($scope.course);
-        $http({
-            method: 'POST',
-            url: '/api/courses',
-            data: d,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+            if ($scope.tagsRaw) {
+                $scope.course.tags = JSON.stringify($scope.tagsRaw);
             }
-        })
-            .success(function(data) {
-                console.log(data);
-                if(data.result) {
-                    $scope.$emit('onAfterCreateNewCourse');
-                    window.location.href = '/course/' + data.course.slug + '/#/cid/' + data.course._id + '?new=1';
-                } else {
-                    if( data.result != null && !data.result){
-                        $scope.errorName = data.errors;
-                        console.log(data.errors);
-                    }
+
+            $scope.course.category = $scope.$parent.category._id;
+
+            var d = transformRequest($scope.course);
+            $http({
+                method: 'POST',
+                url: '/api/courses',
+                data: d,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
                 }
-            }) ;
+            })
+                .success(function (data) {
+                    console.log(data);
+                    if (data.result) {
+                        $scope.$emit('onAfterCreateNewCourse');
+                        window.location.href = '/course/' + data.course.slug + '/#/cid/' + data.course._id + '?new=1';
+                    } else {
+                        if (data.result != null && !data.result) {
+                            $scope.errors = data.errors;
+                            console.log(data.errors);
+                        }
+                    }
+                });
+        }
     };
 });
