f6b93b412b08f5b1b2fb52831f938381672c0d52
Iraklis Dimitriadis <rakli@comtip.net>
2015-11-28 00:41:52 +0100

    broken

--- public/angular/courseMapper.js
+++ public/angular/courseMapper.js
@@ -2490,7 +2490,56 @@ app.directive('pwCheck', [function () {
             });
         }
     };
-}]);;function Spinner($timeout) {
+}]);;app.directive('simplecolorpicker', function() {
+  return {
+    restrict: 'A',
+
+    link: function(scope, element, attrs, ngModel) {
+      var colorPicker = null;
+      var initialSelectedColor = null;
+
+      function selectColor(color) {
+        initialSelectedColor = null;
+        element.val(color);
+        element.simplecolorpicker('selectColor', element.val());
+      }
+
+      // HACK Wait for the AngularJS expressions inside element to be compiled
+      setTimeout(function() {
+        colorPicker = element.simplecolorpicker();
+        if (initialSelectedColor !== null) {
+          // Initializes the colorpicker with a color if one exists
+          selectColor(initialSelectedColor);
+        }
+
+        // View -> model
+        colorPicker.on('change', function() {
+          scope.$apply(function() {
+            ngModel.$setViewValue(element.val());
+          });
+        });
+      }, 0); // Works with no delay
+
+      // Model -> view
+      ngModel.$render = function() {
+        if (colorPicker !== null) {
+          selectColor(ngModel.$viewValue);
+        } else {
+          initialSelectedColor = ngModel.$viewValue;
+        }
+      };
+
+      // Cleanup
+      element.on('$destroy', function() {
+        if (colorPicker !== null) {
+          element.simplecolorpicker('destroy');
+        }
+      });
+
+    }
+  };
+});
+;function Spinner($timeout) {
     return {
         restrict: 'E',
         template: '<i class="fa fa-cog fa-spin"></i>',
