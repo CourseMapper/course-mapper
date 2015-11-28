68b06a616775c84178ff11d63bfe129b4a2c98bc
Johannes Klöckner <johanneskloeckner@web.de>
2015-11-28 03:31:12 +0100

    jhu gjmn f  bfd mfndb nbd

--- public/angular/courseMapper.js
+++ public/angular/courseMapper.js
@@ -2191,15 +2191,28 @@ app.directive('movablePdf', function() {
             templateUrl: '/angular/views/pdf-annotation-zone.html',
             //replace: true,
             //transclude: true,
-            controller: function($http, $scope, $rootScope, $sce){
+            controller: function($http, $scope, $rootScope, $sce, $timeout){
               $scope.canMove = true;
               $scope.switchShowAnnoZones = 'On';
               $scope.annZoneID = rectPrefix + divCounter;
+              $scope.annZoneId = "";
               $scope.opacityFactorHighlight = "0.75";
               $scope.tagName = "";
               $scope.color ="#444444";
               $scope.dataRelCoord = "100;100";
               $scope.colorPickerId ="1";
+              $scope.divCounter = "1";
+              $scope.color = "";
+              $scope.editZoneMode = "BLUB";
+
+
+
+              $scope.setEditZoneMode = function(a,b,c){;};
+
+              $timeout(function(){
+                $scope.$apply();
+              });
+
 
               $('select[name="colorpicker-change-background-color"]').simplecolorpicker({picker: true, theme: 'glyphicons'});
               $('#destroy').on('click', function() {
@@ -2207,6 +2220,8 @@ app.directive('movablePdf', function() {
               });
               // By default, activate simplecolorpicker plugin on HTML selects
               $('#init').trigger('click');
+
+
             }
         };
     }
