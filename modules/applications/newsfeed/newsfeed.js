c5bdbe36e40061f784a2822d4da5304565ec436d
Zuhra Sofyan <zuhrasofyan@gmail.com>
2015-11-30 01:07:51 +0100

    Add event handler for onAfterSubTopicCreated, onAfterSubTopicEdited, onAfterContentNodeCreated, onAfterContentNodeEdited Hook listener

--- modules/applications/newsfeed/newsfeed.js
+++ modules/applications/newsfeed/newsfeed.js
@@ -1,7 +1,7 @@
 var debug = require('debug')('cm:server');
 var NewsfeedAgg = require('./models/newsfeed.model.js');
 var Votes = require('../../votes/models/votes.js');
-//var Discussion = require('../../discussion/models/courseDiscussions.js');
+var SubTopics = require('../../trees/treeNodes.js');
 var Post = require('../../discussion/models/posts.js');
 
 var NewsfeedListener = {
@@ -19,12 +19,14 @@ var NewsfeedListener = {
                             .exec(function (err, result) {
                                 if (result) {
                                     var courseId = result.course;
+                                    var postName = result.title;
                                     if (courseId) {
                                         var nf = new NewsfeedAgg(
                                             {
                                                 userId: newVote.createdBy,
                                                 actionSubjectIds: postId,
                                                 actionSubject: "vote",
+                                                actionName: postName,
                                                 courseId: courseId,
                                                 actionType: "added",
                                                 dateAdded: newVote.dateUpdated
@@ -47,6 +49,7 @@ var NewsfeedListener = {
                                                                 userId: newVote.createdBy,
                                                                 actionSubjectIds: postId,
                                                                 actionSubject: "vote",
+                                                                actionName: parentResult.title,
                                                                 courseId: parentResult.course,
                                                                 actionType: "added",
                                                                 dateAdded: newVote.dateUpdated
@@ -71,19 +74,134 @@ var NewsfeedListener = {
 
                 }
             });
-        /*
-         var nf = new NewsfeedAgg(
-         {userId: newVote.createdBy, actionSubjectIds: postId , actionSubject: "vote", courseId: courseId,  actionType : "added", dateAdded: newVote.dateUpdated}
-         );
-         nf.save(
 
-         function(err, doc){
-         if(!err) debug('');
-         else
-         debug(err);
-         });
-         */
+    },
+
+    onAfterSubTopicCreated: function (newSubTopic) {
+        SubTopics.findOne({_id: newSubTopic._id})
+            .exec(function (err, doc) {
+                if (doc) {
+                    var courseId = doc.courseId;
+                    if (courseId) {
+                        var nf = new NewsfeedAgg(
+                            {
+                                userId: doc.createdBy,
+                                actionSubjectIds: doc.id,
+                                actionSubject: "sub topic",
+                                actionName : doc.name,
+                                courseId: courseId,
+                                actionType: "created",
+                                dateAdded: doc.dateUpdated
+                            }
+                        );
+                        nf.save(
+                            function (err, doc) {
+                                if (!err) debug('');
+                                else
+                                    debug(err);
+                            }
+                        );
+                    }
+
+                }
+            });
+
+    },
+
+    onAfterSubTopicEdited: function (editSubTopic) {
+        SubTopics.findOne({_id: editSubTopic._id})
+            .exec(function (err, doc) {
+                if (doc) {
+                    var courseId = doc.courseId;
+                    if (courseId) {
+                        var nf = new NewsfeedAgg(
+                            {
+                                userId: doc.createdBy,
+                                actionSubjectIds: doc.id,
+                                actionSubject: "sub topic",
+                                actionName : doc.name,
+                                courseId: courseId,
+                                actionType: "edited",
+                                dateAdded: doc.dateUpdated
+                            }
+                        );
+                        nf.save(
+                            function (err, doc) {
+                                if (!err) debug('');
+                                else
+                                    debug(err);
+                            }
+                        );
+                    }
+
+                }
+            });
+
+    },
+
+    onAfterContentNodeCreated: function (newContentNode) {
+        SubTopics.findOne({_id: newContentNode._id})
+            .exec(function (err, doc) {
+                if (doc) {
+                    var courseId = doc.courseId;
+                    if (courseId) {
+                        var nf = new NewsfeedAgg(
+                            {
+                                userId: doc.createdBy,
+                                actionSubjectIds: doc.id,
+                                actionSubject: "content node",
+                                actionName: doc.name,
+                                courseId: courseId,
+                                actionType: "created",
+                                dateAdded: doc.dateUpdated
+                            }
+                        );
+                        nf.save(
+                            function (err, doc) {
+                                if (!err) debug('');
+                                else
+                                    debug(err);
+                            }
+                        );
+                    }
+
+                }
+            });
+
+    },
+
+    onAfterContentNodeEdited: function (editContentNode) {
+        SubTopics.findOne({_id: editContentNode._id})
+            .exec(function (err, doc) {
+                if (doc) {
+                    var courseId = doc.courseId;
+                    if (courseId) {
+                        var nf = new NewsfeedAgg(
+                            {
+                                userId: doc.createdBy,
+                                actionSubjectIds: doc.id,
+                                actionSubject: "content node",
+                                actionName: doc.name,
+                                courseId: courseId,
+                                actionType: "edited",
+                                dateAdded: doc.dateUpdated
+                            }
+                        );
+                        nf.save(
+                            function (err, doc) {
+                                if (!err) debug('');
+                                else
+                                    debug(err);
+                            }
+                        );
+                    }
+
+                }
+            });
+
     }
+
+
 };
 
 module.exports = NewsfeedListener;
