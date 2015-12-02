7b50f2d12882b3369d54284cf23a81e0d844b306
RPL <r@rpl.im>
2015-12-02 16:31:34 +0100

    added ng-cloack and some usabilty fix in admin

--- frontend-modules/angular-admin/route.js
+++ frontend-modules/angular-admin/route.js
@@ -1,69 +1,84 @@
 admin.config(['$routeProvider',
-    function($routeProvider) {
+    function ($routeProvider) {
         $routeProvider.
-            when('/categories', {
-                templateUrl: '/cm-admin/categories',
-                controller: 'adminController',
-                resolve: {
-                    pd: function( $q ) {
-                        return( {
-                            title: 'Manage Categories',
-                            breads: [
-                                {a: '', active: true, title: 'Categories'}
-                            ]
-                        } );
-                    }
+        when('/categories', {
+            templateUrl: '/cm-admin/categories',
+            controller: 'adminController',
+            resolve: {
+                pd: function ($q) {
+                    return ( {
+                        title: 'Manage Categories',
+                        breads: [
+                            {a: '', active: true, title: 'Categories'}
+                        ]
+                    } );
                 }
-            }).
+            }
+        }).
 
-            when('/categories/:category', {
-                templateUrl: '/cm-admin/category',
-                controller: 'adminController',
-                resolve: {
-                    pd: function( $q ) {
-                        return( {
-                            title: 'Manage Category',
-                            breads: [
-                                {a: '#/categories', active: false, title: 'Categories'}
-                            ]
-                        });
-                    }
+        when('/categories/:category', {
+            templateUrl: '/cm-admin/category',
+            controller: 'adminController',
+            resolve: {
+                pd: function ($q) {
+                    return ( {
+                        title: 'Manage Category',
+                        breads: [
+                            {a: '#/categories', active: false, title: 'Categories'}
+                        ]
+                    });
                 }
-            }).
+            }
+        }).
 
-            when('/widgets', {
-                templateUrl: '/cm-admin/applications',
-                controller: 'applicationsController',
-                resolve: {
-                    pd: function( $q ) {
-                        return( {
-                            title: 'Manage Widgets',
-                            breads: [
-                                {a: '#/widgets', active: false, title: 'Widgets'}
-                            ]
-                        });
-                    }
+        when('/widgets', {
+            templateUrl: '/cm-admin/applications',
+            controller: 'applicationsController',
+            resolve: {
+                pd: function ($q) {
+                    return ( {
+                        title: 'Manage Widgets',
+                        breads: [
+                            {a: '#/widgets', active: false, title: 'Widgets'}
+                        ]
+                    });
                 }
-            }).
+            }
+        }).
 
-            when('/tabs', {
-                templateUrl: '/cm-admin/tabs',
-                controller: 'tabsController',
-                resolve: {
-                    pd: function( $q ) {
-                        return( {
-                            title: 'Manage Tabs',
-                            breads: [
-                                {a: '#/tabs', active: false, title: 'Tabs'}
-                            ]
-                        });
-                    }
+        when('/tabs', {
+            templateUrl: '/cm-admin/tabs',
+            controller: 'tabsController',
+            resolve: {
+                pd: function ($q) {
+                    return ( {
+                        title: 'Manage Tabs',
+                        breads: [
+                            {a: '#/tabs', active: false, title: 'Tabs'}
+                        ]
+                    });
                 }
-            }).
+            }
+        }).
 
-            otherwise({
-                redirectTo: '/cm-admin'
-            });
+        when('/cm-admin', {
+            templateUrl: '/cm-admin-html.tpl',
+            controller: 'adminHomeController',
+            resolve: {
+                pd: function ($q) {
+                    return ( {
+                        title: 'Admin Home',
+                        breads: [
+                            {a: '#/home', active: false, title: 'Home'}
+                        ]
+                    });
+                }
+            }
+        }).
+
+        otherwise({
+            redirectTo: '/cm-admin'
+        });
     }]);
 
 
