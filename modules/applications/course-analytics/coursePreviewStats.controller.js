843ecef298c70b23d772cf27c12daf37c8ab2541
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-01-10 08:33:23 +0100

    Widget - Create course-stats API to count total enrolledUser, total PDF+video files, and discussion

--- modules/applications/course-analytics/coursePreviewStats.controller.js
+++ modules/applications/course-analytics/coursePreviewStats.controller.js
@@ -2,6 +2,7 @@ var config = require('config');
 var UserCourses = require('../../catalogs/userCourses.js');
 var Courses = require('../../catalogs/courses.js');
 var Resources = require('../../trees/resources.js');
+var Posts = require('../../discussion/models/posts.js');
 var mongoose = require('mongoose');
 var debug = require('debug')('cm:db');
 var appRoot = require('app-root-path');
@@ -28,6 +29,59 @@ coursePreviewStats.prototype.getTotalUser = function (error, params, done) {
         else {
             done(res);
 
+        }
+
+    });
+};
+
+coursePreviewStats.prototype.getTotalVideo = function (error, params, done) {
+    if (!helper.checkRequiredParams(params, ['courseId'], error)) {
+        return;
+    }
+
+    //var self = this;
+    params.courseId = mongoose.Types.ObjectId(params.courseId);
+    params.type = 'pdf';
+
+    Resources.find({courseId:params.courseId, type:{$ne: params.type}}).count().exec(function (err, res) {
+        if (err) error(err);
+        else {
+            done(res);
+        }
+
+    });
+};
+
+coursePreviewStats.prototype.getTotalPdf = function (error, params, done) {
+    if (!helper.checkRequiredParams(params, ['courseId'], error)) {
+        return;
+    }
+
+    //var self = this;
+    params.courseId = mongoose.Types.ObjectId(params.courseId);
+    params.type = 'pdf';
+
+    Resources.find(params).count().exec(function (err, res) {
+        if (err) error(err);
+        else {
+            done(res);
+        }
+
+    });
+};
+
+coursePreviewStats.prototype.getTotalDiscussion = function (error, params, done) {
+    if (!helper.checkRequiredParams(params, ['course'], error)) {
+        return;
+    }
+
+    //var self = this;
+    params.course = mongoose.Types.ObjectId(params.course);
+
+    Posts.find(params).count().exec(function (err, res) {
+        if (err) error(err);
+        else {
+            done(res);
         }
 
     });
--- public/course-analytics/ca-preview-statistics.js
+++ public/course-analytics/ca-preview-statistics.js
@@ -7,10 +7,25 @@ angular.module('PreviewStats', [''])
         //alert(parseLoc);
         if (parseLoc) {
 
-            $http.get('/api/course-analytics/' + parseLoc).success(function (data) {
+            /*$http.get('/api/course-analytics/' + parseLoc).success(function (data) {
                 //$scope.data = data;
                 $scope.userList = data.courses;
 
+            });*/
+            $http.get('/api/course-analytics/course-stats/total-user/' + parseLoc).success(function (data) {
+                $scope.totalUser = data.userEnrolled;
+            });
+
+            $http.get('/api/course-analytics/course-stats/total-video/' + parseLoc).success(function (data) {
+                $scope.totalVideo = data.totalVideo;
+            });
+
+            $http.get('/api/course-analytics/course-stats/total-pdf/' + parseLoc).success(function (data) {
+                $scope.totalPdf = data.totalPdf;
+            });
+
+            $http.get('/api/course-analytics/course-stats/total-discussion/' + parseLoc).success(function (data) {
+                $scope.totalDiscussion = data.totalDiscussion;
             });
 
             $http.get('/api/treeNodes/course/' + parseLoc).success(function(data){
@@ -20,6 +35,8 @@ angular.module('PreviewStats', [''])
             $http.get('/api/discussions/' + parseLoc).success(function(data){
                 $scope.discussions = data.posts;
             });
+
+
         } else
             console.log('cannot get courseId in analytic widget');
 
