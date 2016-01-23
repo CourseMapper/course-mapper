28a84b7e7e7e918b00949970fd9bc124a804266b
RPL <r@rpl.im>
2016-01-23 17:25:41 +0100

    can reset password

--- public/angular/courseMapper.js
+++ public/angular/courseMapper.js
@@ -537,7 +537,7 @@ app.controller('NewCourseController', function($scope, $filter, $http, $location
         $scope.currentTab = q.tab;
 
         $timeout(function () {
-            if (!authService.isLoggedIn) {
+            if (!authService.isLoggedIn && $scope.currentTab != $scope.defaultPath) {
                 authService.showLoginForm();
             }
         }, 120);
@@ -2230,6 +2230,91 @@ app.controller('AppSettingController', function(  Page) {
                         name: '',
                         description: ''
                     }
+
+                });
+        }
+    };
+});
+;app.controller('ResetPasswordController', function ($scope, $filter, $http, toastr, Page, $timeout) {
+    $scope.submitted = false;
+    $scope.isLoading = false;
+    $scope.errors = [];
+    $scope.loginData = {};
+
+    Page.setTitleWithPrefix('Reset Password');
+
+    $scope.resetPassword = function (isValid) {
+        var url = window.location.href.split('/');
+        var token = url[url.length - 1];
+
+        if (isValid) {
+            $scope.isLoading = true;
+            var d = transformRequest($scope.loginData);
+            $http({
+                method: 'POST',
+                url: '/api/accounts/reset/' + token,
+                data: d,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+                .success(function (data) {
+                    if (data.result) {
+                        toastr.success('Your password has been reset, Please login with your new password');
+                    }
+
+                    $scope.isLoading = false;
+                    $scope.loginData = {};
+
+                    $scope.resetForm.$setPristine();
+
+                    $timeout(function () {
+                        window.location.href = '/accounts/login';
+                    }, 500);
+                })
+                .error(function (data) {
+                    $scope.isLoading = false;
+                    if (data.errors) {
+                        $scope.errors = data.errors;
+                    }
+                    toastr.success('Sending Request failed');
+                    $scope.loginData = {};
+
+                });
+        }
+    };
+
+    $scope.requestReset = function (isValid) {
+        if (isValid) {
+
+            $scope.isLoading = true;
+            var d = transformRequest($scope.loginData);
+            $http({
+                method: 'POST',
+                url: '/api/accounts/resetPassword',
+                data: d,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+                .success(function (data) {
+                    if (data.result) {
+                        toastr.success('Reset Password Requested, Please check your email');
+                    }
+
+                    $scope.isLoading = false;
+                    $scope.loginData = {};
+
+                    $scope.resetForm.$setPristine();
+                })
+                .error(function (data) {
+                    $scope.isLoading = false;
+                    if (data.errors) {
+                        $scope.errors = data.errors;
+                    }
+
+                    toastr.error('Sending Request failed');
+                    $scope.loginData = {};
 
                 });
         }
