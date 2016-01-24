5255e495ee192a0cad7d78273921793f26abb78e
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-01-24 19:04:06 +0100

    Newsfeed styling and add filter

--- public/newsfeed/newsfeed.controller.js
+++ public/newsfeed/newsfeed.controller.js
@@ -3,8 +3,25 @@ app.controller('NewsfeedController', function ($scope, $rootScope, $filter, $htt
                                              courseService, authService, toastr, Page) {
     $scope.courseId = $routeParams.courseId;
 
+    $scope.newsfeedData = [];
+    $scope.filterDropDown = {};
+    //$scope.query = "vote";
+    $scope.nfType = [
+        {"name": "course"},
+        {"name": "vote"},
+        {"name": "sub topic"},
+        {"name": "content node"},
+        {"name": "node"},
+        {"name": "pdf annotation"},
+        {"name": "pdf annotation zone"},
+        {"name": "video annotation"},
+        {"name": "link"},
+        {"name": "discussion"}
+    ];
+
     $http.get('/api/newsfeed/'+$scope.courseId).success(function (data) {
         $scope.newsfeedData = data.newsfeeds;
+        $scope.nfLength = data.newsfeeds.length;
     });
 
 
