bd639a815380b94bb6a0e445eb169d2bc436166f
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-02-07 03:50:38 +0100

    Widget - Add total link to node-detail/icon-analytics widget

--- modules/applications/content-node-detail/icon-analytics.js
+++ modules/applications/content-node-detail/icon-analytics.js
@@ -1,6 +1,8 @@
 var appRoot = require('app-root-path');
 var mongoose = require('mongoose');
 var Courses = require(appRoot + '/modules/catalogs/courses.js');
+var TN = require(appRoot + '/modules/trees/treeNodes.js');
+var Links = require(appRoot + '/modules/links/models/links.js');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 
@@ -8,26 +10,40 @@ function AStat(){
 }
 
 AStat.prototype.init = function(params){
-    this._id = mongoose.Types.ObjectId(params.cid);
+    this._id = mongoose.Types.ObjectId(params.nodeId);
     //console.log(this._id)
 };
 
 AStat.prototype.run = async ( function(){
     var self = this;
 
-    var result = await( Courses.findOne({
+    var result = await( TN.findOne({
         _id: self._id
     }).exec());
 
-    self.result = result;
+    var arrResources = result.resources;
+    var contentId = result._id
+
+    var totalLinks = await(Links.find({
+        contentNode: contentId
+    }).count().exec());
+    //var arrResourcesLength = arrResources.length;
+
+    /*for (var i=0; i < arrResourcesLength; i++) {
+
+
+    }*/
+
+    //self.result = result;
+    self.test = arrResources;
+    self.tl = totalLinks;
 } );
 
 
 AStat.prototype.render = function(){
     return  '<div class="icon-stat" style="text-align: center">' +
                 '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-pencil-square" style="font-size: 12px"></i> '+ '-' +'</div> '+
-                '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-link" style="font-size: 12px"></i> '+ '-' +'</div> ' +
-                '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-heart" style="font-size: 12px"></i> '+ '0' +'</div> ' +
+                '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-link" style="font-size: 12px"></i> '+ this.tl +'</div> ' +
             '</div>';
 };
 
