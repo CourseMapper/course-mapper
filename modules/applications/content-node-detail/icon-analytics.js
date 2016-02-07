5833b2091e55a0b5b13b41d1815e8003b24687cb
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-02-07 14:00:42 +0100

    test

--- modules/applications/content-node-detail/icon-analytics.js
+++ modules/applications/content-node-detail/icon-analytics.js
@@ -2,7 +2,10 @@ var appRoot = require('app-root-path');
 var mongoose = require('mongoose');
 var Courses = require(appRoot + '/modules/catalogs/courses.js');
 var TN = require(appRoot + '/modules/trees/treeNodes.js');
+var Resources = require(appRoot + '/modules/trees/resources.js');
 var Links = require(appRoot + '/modules/links/models/links.js');
+var PdfAnnotation = require(appRoot + '/modules/slide-viewer/annotation.js');
+var VideoAnnotation = require(appRoot + '/modules/annotations/video-annotation.js');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 
@@ -17,32 +20,50 @@ AStat.prototype.init = function(params){
 AStat.prototype.run = async ( function(){
     var self = this;
 
-    var result = await( TN.findOne({
+    var contentNodeDetail = await( TN.findOne({
         _id: self._id
     }).exec());
 
-    var arrResources = result.resources;
-    var contentId = result._id
+    var arrResources = contentNodeDetail.resources;
+    var contentId = contentNodeDetail._id;
 
     var totalLinks = await(Links.find({
         contentNode: contentId
     }).count().exec());
-    //var arrResourcesLength = arrResources.length;
 
-    /*for (var i=0; i < arrResourcesLength; i++) {
+    var arrResourcesLength = arrResources.length;
+
+    for (var i=0; i < arrResourcesLength; i++) {
+        var a = await(Resources.findOne({
+            _id:arrResources[i]
+        }).exec());
+        if (a.type="video") {
+            var sumAnnoVideo = await(VideoAnnotation.find({
+                video_id: a._id
+            }).count().exec());
+        }
+        else {
+            var sumAnnoPdf = await(PdfAnnotation.find({
+                pdfId: a._id
+            }).count().exec());
+        }
 
 
-    }*/
 
-    //self.result = result;
+    }
+    self.arr = arrResources;
+    self.contentNodeDetail = contentNodeDetail;
     self.test = arrResources;
     self.tl = totalLinks;
+    self.sumAnnoPdf = sumAnnoPdf;
+    self.sumAnnoVideo = sumAnnoVideo;
 } );
 
 
 AStat.prototype.render = function(){
     return  '<div class="icon-stat" style="text-align: center">' +
-                '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-pencil-square" style="font-size: 12px"></i> '+ '-' +'</div> '+
+                '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-pencil-square" style="font-size: 12px"></i> '+ ' pdf ' + this.sumAnnoPdf +'</div> '+
+                '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-pencil-square" style="font-size: 12px"></i> '+ ' video ' + this.sumAnnoVideo +'</div> '+
                 '<div class="badge bg-light-blue" style="text-align: center"><i class="fa fa-link" style="font-size: 12px"></i> '+ this.tl +'</div> ' +
             '</div>';
 };
