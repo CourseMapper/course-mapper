0bf9bf1a2726f18db8ceb367811bc6e2b223ff03
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-03-14 22:04:21 +0100

    Newsfeed - Fix onVideoAnnotationDeleted bug

--- modules/applications/newsfeed/newsfeed.js
+++ modules/applications/newsfeed/newsfeed.js
@@ -747,47 +747,44 @@ var NewsfeedListener = {
     },
 
     onAfterVideoAnnotationDeleted: function (deleteVideoAnnotation, user) {
-        VideoAnnotation.findOne({_id:deleteVideoAnnotation})
-            .exec(function(err, doc){
-                if (doc) {
-                    var videoId = doc.video_id;
-                    if (videoId) {
-                        Resources.findOne({_id:videoId})
-                            .exec(function(err, result){
-                                if (result) {
-                                    var treeNodeId = result.treeNodeId;
-                                    if (treeNodeId) {
-                                        SubTopics.findOne({_id:treeNodeId})
-                                            .exec(function(err, res){
-                                                if (res) {
-                                                    var nf = new NewsfeedAgg(
-                                                        {
-                                                            userId: doc.authorId,
-                                                            actionSubjectIds: videoId,
-                                                            actionSubject: "video annotation",
-                                                            actionName: res.name,
-                                                            courseId: result.courseId,
-                                                            nodeId: res.id,
-                                                            actionType: "deleted",
-                                                            dateAdded: doc.date_modified
-                                                        }
-                                                    );
-                                                    nf.save(
-                                                        function (err, doc) {
-                                                            if (!err) debug('');
-                                                            else
-                                                                debug(err);
-                                                        }
-                                                    );
-                                                }
-                                            })
-                                    }
+        var videoId = deleteVideoAnnotation.video_id;
+        var userId = user;
+        var dateAdded = deleteVideoAnnotation.date_modified;
+        Resources.findOne({_id:videoId})
+            .exec(function(err, result){
+                if (result) {
+                    var treeNodeId = result.treeNodeId;
+                    if (treeNodeId) {
+                        SubTopics.findOne({_id: treeNodeId})
+                            .exec(function (err, res) {
+                                if (res) {
+                                    var nf = new NewsfeedAgg(
+                                        {
+                                            userId: userId,
+                                            actionSubjectIds: videoId,
+                                            actionSubject: "video annotation",
+                                            actionName: res.name,
+                                            courseId: result.courseId,
+                                            nodeId: res.id,
+                                            actionType: "deleted",
+                                            dateAdded: dateAdded
+                                        }
+                                    );
+                                    nf.save(
+                                        function (err, doc) {
+                                            if (!err) debug('');
+                                            else
+                                                debug(err);
+                                        }
+                                    );
+
                                 }
                             })
                     }
                 }
             });
     },
+
 
     //Listener for Link
     onAfterLinkCreated: function (newLink) {
