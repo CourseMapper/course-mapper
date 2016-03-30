f5f689da2f17dcb4c12b9ebab0aa8ea83ab76a2e
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-03-30 20:57:43 +0200

    Widget - Course List - basic stats widget - add enrolled user total number stats

---
+++ modules/applications/course-analytics/course-list-basic-stats.js
@@ -0,0 +1,36 @@
+var appRoot = require('app-root-path');
+var mongoose = require('mongoose');
+var moment = require ('moment');
+var Courses = require(appRoot + '/modules/catalogs/courses.js');
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+//var TreeNodes = require(appRoot + '/modules/trees/treeNodes.js');
+var UserCourses = require(appRoot + '/modules/catalogs/userCourses.js');
+
+function CourseListBasicStats(){
+}
+
+CourseListBasicStats.prototype.init = function(params){
+    this._id = mongoose.Types.ObjectId(params.cid);
+};
+
+CourseListBasicStats.prototype.run = async(function(){
+    var self = this;
+    var countEnrolledUser = await( UserCourses.find({
+        course: self._id,
+        isEnrolled: 'true'
+    }).count().exec());
+
+    if (countEnrolledUser) {
+        self.countEnrolledUser = countEnrolledUser;
+    } else {
+        self.countEnrolledUser = 0;
+    }
+});
+
+CourseListBasicStats.prototype.render = function(){
+
+    return '<span class="label label-info"> Total enrolled participants: <b>' + this.countEnrolledUser + '</span> <br>';
+};
+
+module.exports = CourseListBasicStats;
