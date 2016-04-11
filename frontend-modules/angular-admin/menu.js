09781a8c3e1fc1036df0d76e2aebf38eec7549bc
RPL <r@rpl.im>
2016-04-11 22:41:42 +0200

    admin can delete only himself

--- frontend-modules/angular-admin/menu.js
+++ frontend-modules/angular-admin/menu.js
@@ -2,5 +2,7 @@ admin.controller('MainMenuController', function($scope, $http, $rootScope) {
     $http.get('/api/account').success(function(data) {
         $scope.user = data;
         $rootScope.user = data;
+ 
+        $rootScope.$broadcast('onAfterInitUser', $rootScope.user);
     });
 });
