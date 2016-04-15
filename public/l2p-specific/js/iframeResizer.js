6067a3a5712172503046ee4336549287a09b461a
Iraklis Dimitriadis <rakli@comtip.net>
2016-04-15 16:25:26 +0200

    resize window fix

--- public/l2p-specific/js/iframeResizer.js
+++ public/l2p-specific/js/iframeResizer.js
@@ -2,7 +2,8 @@ window.onresize = function(){
     window.parent.postMessage(
     Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
               document.body.scrollHeight, document.body.offsetHeight)
-      ,"*")
+      ,"*");
+      console.log("window resized");
  };
 
 window.onload = function() {
@@ -10,4 +11,13 @@ window.onload = function() {
         Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
                 document.body.scrollHeight, document.body.offsetHeight)
         ,"*");
+        console.log("window onload resized");
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+   window.parent.postMessage(
+   Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
+             document.body.scrollHeight, document.body.offsetHeight)
+     ,"*");
+     console.log("document ready resized");
+});
