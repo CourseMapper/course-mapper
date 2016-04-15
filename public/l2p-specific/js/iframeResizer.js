508cc33584bbaa0600c809276e5528bbbdb217dd
Iraklis Dimitriadis <rakli@comtip.net>
2016-04-15 14:29:29 +0200

    fix for resizer

--- public/l2p-specific/js/iframeResizer.js
+++ public/l2p-specific/js/iframeResizer.js
@@ -1,13 +1,21 @@
 window.onresize = function(){
-             window.parent.postMessage(
-            Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
-                    document.body.scrollHeight, document.body.offsetHeight)
-            ,"*")
-       };
+    window.parent.postMessage(
+    Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
+              document.body.scrollHeight, document.body.offsetHeight)
+      ,"*")
+ };
 
-    window.onload = function() {
-        window.parent.postMessage(
-            Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
-                    document.body.scrollHeight, document.body.offsetHeight)
-            ,"*");
-    };
+window.onload = function() {
+    window.parent.postMessage(
+        Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
+                document.body.scrollHeight, document.body.offsetHeight)
+        ,"*");
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+   window.parent.postMessage(
+   Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
+             document.body.scrollHeight, document.body.offsetHeight)
+     ,"*");
+     console.log("fired iframeResizer");
+});
