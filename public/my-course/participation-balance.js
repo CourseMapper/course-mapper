bf18e4a82dc26070d78ce4b9d41afbff67a81179
Momchil Marinov <momchil@mail.com>
2016-07-03 22:37:46 +0200

    fix pb

--- public/my-course/participation-balance.js
+++ public/my-course/participation-balance.js
@@ -1,56 +1,95 @@
+'use strict';
+
 angular.module('ParticipationBalance', [''])
-    .controller('ParticipationBalanceController', function($scope, $http) {
+  .controller('ParticipationBalanceController', function ($scope, $http) {
 
-        function buildHSL(h, s, l) {
-            return 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
-        }
+    var WeightedValue = function (value, weight) {
+      this.value = value;
+      this.weight = weight;
+    };
 
-        function normalizeValue(value) {
-            if (!value) {
-                return 0.00;
-            } else if (value > 1.0) {
-                return 1.00;
-            } else if (value < 0.0) {
-                return 0.00;
-            }
-            return value;
-        }
+    function buildHSL(h, s, l) {
+      return 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
+    }
 
-        $scope.percentToHSLColor = function(value, colorful) {
-            var normalizedValue = normalizeValue(value);
-            var h, l, s;
-            if (colorful === true) {
-                h = (1.0 - normalizedValue) * 320;
-                l = 120 * normalizedValue;
-                s = normalizedValue ? 50 : 90;
-            } else {
-                h = 202;
-                l = 52;
-                s = 100 - normalizedValue * 50;
-            }
-            return buildHSL(h, l, s);
-        };
+    function normalizeValue(value) {
+      if (!value) {
+        return 0;
+      } else if (value > 1) {
+        return 1;
+      } else if (value < 0) {
+        return 0;
+      }
+      return value;
+    }
 
-        //get participation balance percentage
-        $scope.getPBP = function(node) {
-            return node.totalAnnotations > 0.00 ? (node.userAnnotations / node.totalAnnotations).toFixed(2) : new Number(0.00);
-        };
+    $scope.percentToHSLColor = function (value) {
+      var normalizedValue = normalizeValue(value);
+      var h, s, l;
+      h = (normalizedValue) * 320;
+      s = 70;
+      l = 50;
+      return buildHSL(h, s, l);
+    };
 
-        $scope.expandState = {};
+    $scope.getParticipationBalance = function (node) {
 
-        $scope.setExpandState = function(value) {
-            let states = $scope.expandState;
-            for (let id in states) {
-                states[id] = value;
-            };
-        };
+      var sources = [
+        new WeightedValue($scope.getWatchProgress(node), 1),
+        new WeightedValue($scope.getAnnotationProgress(node), 2),
+        new WeightedValue($scope.getLinksProgress(node), 3)
+      ];
 
-        $scope.init = function() {
-            $http.get('/api/my-course/participation-balance')
-                .then(function(res) {
-                    $scope.courses = res.data;
-                });
-        };
+      var sum = _(sources)
+        .map(function (c) {
+          return c.value * c.weight;
+        })
+        .reduce(function (m, n) {
+          return m + n;
+        }, 0);
 
-        $scope.init();
-    });
+      var count = _(sources)
+        .map(function (c) {
+          return c.weight;
+        })
+        .reduce(function (m, n) {
+          return m + n;
+        });
+
+      return (sum / count);
+    };
+
+    $scope.getLinksProgress = function (node) {
+      return node.totalLinks > 0 ? (node.userLinks / node.totalLinks) : 0;
+    };
+
+    $scope.getAnnotationProgress = function (node) {
+      return node.totalAnnotations > 0 ? (node.userAnnotations / node.totalAnnotations) : 0;
+    };
+
+    $scope.getWatchProgress = function (node) {
+      var wh = node.watchHistory;
+      if (!wh || !wh.totalTime) {
+        return 0;
+      }
+      return (wh.currentTime / wh.totalTime);
+    };
+
+    $scope.expandState = {};
+
+    $scope.setExpandState = function (value) {
+      var states = $scope.expandState;
+      for (let id in states) {
+        states[id] = value;
+      }
+    };
+
+    $scope.init = function () {
+      $http.get('/api/my-course/participation-balance')
+        .then(function (res) {
+          $scope.courses = res.data;
+        });
+    };
+
+    $scope.init();
+  });
