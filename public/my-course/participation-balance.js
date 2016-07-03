f8c056b95e8c2a20b98b0cfe8e559250b0987353
Momchil Marinov <momchil@mail.com>
2016-07-03 14:04:06 +0200

    update pb

--- public/my-course/participation-balance.js
+++ public/my-course/participation-balance.js
@@ -1,61 +1,56 @@
 angular.module('ParticipationBalance', [''])
-  .controller('ParticipationBalanceController', function ($scope, $http) {
+    .controller('ParticipationBalanceController', function($scope, $http) {
 
-    function buildHSL(h, s, l) {
-      return 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
-    }
+        function buildHSL(h, s, l) {
+            return 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
+        }
 
-    function normalizeValue(value) {
-      if (!value) {
-        return 0.0;
-      } else if (value > 1.0) {
-        return 1.0;
-      } else if (value < 0.0) {
-        return 0.0;
-      }
-      return value;
-    }
+        function normalizeValue(value) {
+            if (!value) {
+                return 0.00;
+            } else if (value > 1.0) {
+                return 1.00;
+            } else if (value < 0.0) {
+                return 0.00;
+            }
+            return value;
+        }
 
-    $scope.percentToHSLColor = function (value, colorful) {
-      var normalizedValue = normalizeValue(value);
+        $scope.percentToHSLColor = function(value, colorful) {
+            var normalizedValue = normalizeValue(value);
+            var h, l, s;
+            if (colorful === true) {
+                h = (1.0 - normalizedValue) * 320;
+                l = 120 * normalizedValue;
+                s = normalizedValue ? 50 : 90;
+            } else {
+                h = 202;
+                l = 52;
+                s = 100 - normalizedValue * 50;
+            }
+            return buildHSL(h, l, s);
+        };
 
-      var h, l, s;
-      if (colorful === true) {
-        h = (1.0 - normalizedValue) * 320;
-        l = 120 * normalizedValue;
-        s = normalizedValue ? 50 : 90;
-      }
-      else {
-        h = 202;
-        l = 52;
-        s = 100 - normalizedValue * 50;
-      }
-      return buildHSL(h, l, s);
-    };
+        //get participation balance percentage
+        $scope.getPBP = function(node) {
+            return node.totalAnnotations > 0.00 ? (node.userAnnotations / node.totalAnnotations).toFixed(2) : new Number(0.00);
+        };
 
-    //get participation balance percentage
-    $scope.getPBP = function (node) {
-      return node.totalAnnotations > 0 ? (node.userAnnotations / node.totalAnnotations).toFixed(2) : 0
-    };
+        $scope.expandState = {};
 
-    $scope.expandState = {};
+        $scope.setExpandState = function(value) {
+            let states = $scope.expandState;
+            for (let id in states) {
+                states[id] = value;
+            };
+        };
 
-    $scope.setExpandState = function (value) {
-      var states = $scope.expandState;
-      console.log(states)
+        $scope.init = function() {
+            $http.get('/api/my-course/participation-balance')
+                .then(function(res) {
+                    $scope.courses = res.data;
+                });
+        };
 
-      for (var id in states) {
-        states[id] = value;
-      }
-      console.log(states)
-    };
-
-    $scope.init = function () {
-      $http.get('/api/my-course/participation-balance')
-        .then(function (res) {
-          $scope.courses = res.data;
-        });
-    };
-
-    $scope.init();
-  });
+        $scope.init();
+    });
