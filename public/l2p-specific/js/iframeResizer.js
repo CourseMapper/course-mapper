f9d0491da155d986de6ac56cea02d44d5762fbdc
Iraklis Dimitriadis <rakli@linux.fritz.box>
2016-09-06 22:43:28 +0200

    frame resize

--- public/l2p-specific/js/iframeResizer.js
+++ public/l2p-specific/js/iframeResizer.js
@@ -1,23 +1,26 @@
 window.onresize = function(){
     window.parent.postMessage(
-    Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
-              document.body.scrollHeight, document.body.offsetHeight)
+    Math.max(600, 600,
+        600, 600)
       ,"*");
       console.log("window resized");
  };
 
+//Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
+//    document.body.scrollHeight, document.body.offsetHeight)
+
 window.onload = function() {
     window.parent.postMessage(
-        Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
-                document.body.scrollHeight, document.body.offsetHeight)
+        Math.max(600, 600,
+            600, 600)
         ,"*");
         console.log("window onload resized");
 };
 
 document.addEventListener('DOMContentLoaded', function() {
    window.parent.postMessage(
-   Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
-             document.body.scrollHeight, document.body.offsetHeight)
+       Math.max(600, 600,
+           600, 600)
      ,"*");
      console.log("document ready resized");
 });
