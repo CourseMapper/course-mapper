bbd23bf5c33c92fd753a58798239f007cc8f0e4d
Momchil Marinov <momchil@mail.com>
2016-10-27 23:32:37 +0200

    search and relevance

--- frontend-modules/angular/advanced-search/controller.js
+++ frontend-modules/angular/advanced-search/controller.js
@@ -1,22 +1,33 @@
-app.controller('AdvancedSearchController', function ($rootScope, $scope, $http) {
+app.controller('AdvancedSearchController', function ($scope, $http) {
 
   var search = function (term) {
 
     // Clear previous result
     $scope.result = null;
+    $scope.relevant = null;
 
     // Check if search is empty
     if (!term || term.length == 0) {
       return;
     }
 
-    // Load search results data
     setBusy(true);
+
+    // Find matching results
     $http.get('/api/advanced-search?term=' + term)
       .success(function (data) {
         $scope.result = data;
         setBusy(false);
       });
+
+    //TODO
+    // Find relevant results
+    $http.get('/api/advanced-search?term=' + term)
+      .success(function (data) {
+        $scope.relevant = data;
+        setBusy(false);
+      });
+
   }
 
   var setBusy = function (isBusy) {
@@ -28,6 +39,7 @@ app.controller('AdvancedSearchController', function ($rootScope, $scope, $http) 
     setBusy(false);
     $scope.searchTerm = '';
     $scope.result = null;
+    $scope.relevant = null;
     $scope.search = search;
 
     // Subscribe to menu query text changes
