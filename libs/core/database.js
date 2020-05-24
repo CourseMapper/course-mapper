2cff838e2cc3a0b074c856309f82bb473d13efdf
Ralf Berger <ralf.berger@uni-due.de>
2020-05-24 11:37:57 +0200

    Retry DB connection

--- libs/core/database.js
+++ libs/core/database.js
@@ -1,27 +1,26 @@
 /* jslint node:true */
-
-/**
- * connecting to database, and returns a mongoose instance
- */
-
 var mongoose = require('mongoose');
 var config = require('config');
 
 function database() {
-    this.connectionOptions = process.env.DATABASE || 'mongodb://localhost:27017/course_mapper';
+  this.url = process.env.DATABASE || 'mongodb://localhost:27017/course_mapper';
+  this.options = { server: {
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+  }};
 }
 
 database.prototype.connect = function () {
-    return mongoose.connect(this.connectionOptions);
+  return mongoose.connect(this.url, this.options);
 };
 
 var DB = new database();
 var db = DB.connect();
 
 mongoose.connection.on('error', function dbConnectionFailed(err) {
-    console.log('Database error: %s', err);
+  console.log('Database error: %s', err);
 }).once('open', function dbConnectionOpened() {
-    console.log('Database connected to "%s".', process.env.DATABASE);
+  console.log('Database connected to "%s".', process.env.DATABASE);
 });
 
 module.exports = db;
